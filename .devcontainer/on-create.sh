4ca3b79be74de4903dada3489e32a0e808929085
Justin Yoo <justin.yoojh@gmail.com>
2024-06-03 05:15:20 +0000

    Add devcontainer settings

---
+++ .devcontainer/on-create.sh
@@ -0,0 +1,13 @@
+## Install additional apt packages
+sudo apt-get update && \
+    sudo apt upgrade -y && \
+    sudo apt-get install -y dos2unix libsecret-1-0 xdg-utils && \
+    sudo apt clean -y && \
+    sudo rm -rf /var/lib/apt/lists/*
+
+## Configure git
+git config --global pull.rebase false
+git config --global core.autocrlf input
+
+## Enable local HTTPS for .NET
+dotnet dev-certs https --trust
