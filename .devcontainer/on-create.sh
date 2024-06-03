00a489e0fd9ef0980b7a315890588e381792d66a
Justin Yoo <justin.yoojh@gmail.com>
2024-06-03 18:50:46 +0900

    Update GHA workflow for CSC verification (#169)

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -11,3 +11,15 @@ git config --global core.autocrlf input
 
 ## Enable local HTTPS for .NET
 dotnet dev-certs https --trust
+
+## CaskaydiaCove Nerd Font
+mkdir -p ~/.local/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaCode.zip
+unzip CascadiaCode.zip -d ~/.local/share/fonts
+rm CascadiaCode.zip
+
+## OH-MY-POSH ##
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+mkdir -p ~/.config/powershell
+echo "oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/powerlevel10k_rainbow.omp.json' | Invoke-Expression" > ~/.config/powershell/Microsoft.PowerShell_profile.ps1
