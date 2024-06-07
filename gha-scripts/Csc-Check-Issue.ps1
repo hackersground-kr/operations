b17cf4aba62f4cdc620a20e581a6e275426f364a
Jihyeon Hwang <104641422+zihyeon08@users.noreply.github.com>
2024-06-07 10:20:29 +0000

    debug

--- gha-scripts/Csc-Check-Issue.ps1
+++ gha-scripts/Csc-Check-Issue.ps1
@@ -73,7 +73,12 @@ if (($eventName -eq "workflow_dispatch") -and ([string]::IsNullOrWhiteSpace($acc
 
 $body = ""
 if ($eventName -eq "workflow_dispatch") {
-    $GitHubPayload = $(gh api /repos/$($(GitHubPayload.repository))/issues/$IssueNumber | ConvertFrom-Json)
+    $apiUrl = "https://api.github.com/repos/$($GitHubPayload.repository)/issues/$IssueNumber"
+    $headers = @{
+        "Authorization" = "Bearer $accessToken"
+    }
+    $GitHubPayload = Invoke-RestMethod -Uri $apiUrl -Headers $headers -Method Get
+    #$GitHubPayload = $(gh api /repos/$($GitHubPayload.repository)/issues/$IssueNumber | ConvertFrom-Json)
     $body = $GitHubPayload.body
 } else {
     $body = $GitHubPayload.event.issue.body
