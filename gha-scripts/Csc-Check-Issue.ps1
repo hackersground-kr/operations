1869e675ee9f40b1664ec0295c93a48445cbbcdd
Jihyeon Hwang <104641422+zihyeon08@users.noreply.github.com>
2024-06-07 15:36:24 +0000

    debug

--- gha-scripts/Csc-Check-Issue.ps1
+++ gha-scripts/Csc-Check-Issue.ps1
@@ -67,17 +67,12 @@ if (($eventName -eq "workflow_dispatch") -and ([string]::IsNullOrWhiteSpace($acc
     Exit 0
 }
 
-throw "/repos/$($GitHubPayload.repository)/issues/$IssueNumber"
-
 $body = ""
 if ($eventName -eq "workflow_dispatch") {
     $GitHubPayload = $(gh api /repos/$($GitHubPayload.repository)/issues/$IssueNumber) | ConvertFrom-Json
     $body = $GitHubPayload.body
 } else {
     $body = $GitHubPayload.event.issue.body
-}
-if (-not $body) {
-    throw "GitHub body is null or empty"
 }
 
 $title = $GitHubPayload.title
