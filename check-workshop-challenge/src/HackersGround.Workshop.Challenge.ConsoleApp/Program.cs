1ca52e646ee028d4f1f1a06f0877653dcab26aa1
Justin Yoo <justin.yoojh@gmail.com>
2024-06-29 17:33:28 +0900

    Add workshop checker app

---
+++ check-workshop-challenge/src/HackersGround.Workshop.Challenge.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using HackersGround.Workshop.Challenge.ConsoleApp.Services;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+               .UseConsoleLifetime()
+               .ConfigureServices(services =>
+               {
+                   services.AddHttpClient<IChallengeCheckerService, ChallengeCheckerService>();
+               })
+               .Build();
+
+var service = host.Services.GetRequiredService<IChallengeCheckerService>();
+await service.RunAsync(args);
