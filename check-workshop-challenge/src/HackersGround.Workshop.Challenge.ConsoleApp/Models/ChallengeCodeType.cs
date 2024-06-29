6c79d7fd7c222c6b0245d37741e24e83e944ae3b
Justin Yoo <justin.yoojh@gmail.com>
2024-06-29 17:34:39 +0900

    Add workshop checker app (#253)

---
+++ check-workshop-challenge/src/HackersGround.Workshop.Challenge.ConsoleApp/Models/ChallengeCodeType.cs
@@ -0,0 +1,32 @@
+﻿namespace HackersGround.Workshop.Challenge.ConsoleApp.Models;
+
+/// <summary>
+/// This defines the challenge code type.
+/// </summary>
+public enum ChallengeCodeType
+{
+    /// <summary>
+    /// Indicates the challenge code is undefined.
+    /// </summary>
+    Undefined,
+
+    /// <summary>
+    /// Indicates the challenge code is Workshop 1.
+    /// </summary>
+    Workshop_1,
+
+    /// <summary>
+    /// Indicates the challenge code is Workshop 2.
+    /// </summary>
+    Workshop_2,
+
+    /// <summary>
+    /// Indicates the challenge code is Workshop 3.
+    /// </summary>
+    Workshop_3,
+
+    /// <summary>
+    /// Indicates the challenge code is Workshop 4.
+    /// </summary>
+    Workshop_4,
+}
