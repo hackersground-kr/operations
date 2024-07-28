d89d59f221990c22f7e00e647ff530435303122f
Jihyeon Hwang <104641422+zihyeon08@users.noreply.github.com>
2024-07-28 14:29:02 +0900

    Update GHA workflow for Workshop submition & completion (#330)

--- check-workshop-challenge/src/HackersGround.Workshop.Challenge.ConsoleApp/Models/ChallengeCodeType.cs
+++ check-workshop-challenge/src/HackersGround.Workshop.Challenge.ConsoleApp/Models/ChallengeCodeType.cs
@@ -13,20 +13,5 @@ public enum ChallengeCodeType
     /// <summary>
     /// Indicates the challenge code is Workshop 1.
     /// </summary>
-    Workshop_1,
-
-    /// <summary>
-    /// Indicates the challenge code is Workshop 2.
-    /// </summary>
-    Workshop_2,
-
-    /// <summary>
-    /// Indicates the challenge code is Workshop 3.
-    /// </summary>
-    Workshop_3,
-
-    /// <summary>
-    /// Indicates the challenge code is Workshop 4.
-    /// </summary>
-    Workshop_4,
+    Workshop,
 }
